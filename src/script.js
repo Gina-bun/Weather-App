e1f545783d9308b7eab8d097e6c8b900b7f6b3b6
Gina-bun <64642337+Gina-bun@users.noreply.github.com>
2026-05-23 23:08:59 +0000

    feat: add weather display, wiring api to display

--- src/script.js
+++ src/script.js
@@ -1,17 +1,53 @@
+const searchInput = document.getElementById("search-input");
+const searchIcon = document.getElementById("search-icon");
+const currentLocation = document.getElementById("location");
+const weatherIcon = document.getElementById("weather-icon");
+const weatherDescription = document.getElementById("weather-desc");
+const temperature = document.getElementById("temperature");
+const day = document.getElementById("day");
+const humidity = document.getElementById("humidity");
 
+async function getWeather(city) {
+  const apiKey = "8038ecd13485738574daee006d794fac";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-async function getWeather(city){
-    const apiKey = "8038ecd13485738574daee006d794fac"
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
 
-    try {
-        const response = await fetch(url)
-        const data = await response.json()
-        console.log(data)
-    } 
-    catch (e) {
-        console.error("Error in data fetching: ", e)
+    if (data.cod === "404") {
+      currentLocation.textContent = "City not found"
+      temperature.textContent = ""
+      weatherDescription.textContent = ""
+      weatherIcon.src = ""
+      day.textContent = ""
+      humidity.textContent = ""
+
+      return
     }
+
+    //display weather when fetch succeeds
+    displayWeather(data);
+  } catch (e) {
+    console.error("Error fetching weather: ", e)
+  }
 }
 
-getWeather("Accra")
+function displayWeather(data) {
+  const today = new Date();
+  const weekDay = today.toLocaleDateString("en-US", { weekday: "long" });
+
+  currentLocation.textContent = `${data.name}`;
+  temperature.textContent = `${data.main.temp}°C`;
+  weatherDescription.textContent = data.weather[0].description;
+  weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  day.textContent = `${weekDay}`;
+  humidity.textContent = `Humidity: ${data.main.humidity}%`;
+}
+
+searchInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    const city = e.target.value;
+    getWeather(city);
+  }
+});
